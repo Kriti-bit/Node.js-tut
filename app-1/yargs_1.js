2a3df787842c00cf9fa19e79899c4722db31d5c6
Kriti <69788174+Kriti-bit@users.noreply.github.com>
2020-11-13 16:33:35 +0530

    Create yargs_1.js

---
+++ app-1/yargs_1.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk')
+const validator=require('validator')
+const yargs=require('yargs')
+
+yargs.command({
+    command : 'add',
+    describe : 'Adding a new note',
+    handler : function(){
+        console.log('Adding new note')
+    }
+})
+
+yargs.command({
+    command : 'remove',
+    describe : 'Removing a note',
+    handler : function(){
+        console.log('Removing the last note')
+    }
+
+})
+yargs.parse()
