75aaa2e6134232ac52cdb26472d4c43571354284
Kriti <69788174+Kriti-bit@users.noreply.github.com>
2020-11-22 22:50:00 +0530

    Create Asynchronous.js

---
+++ weather-app/Asynchronous.js
@@ -0,0 +1,14 @@
+//Asynchronous Node.js
+console.log("Starting Up")
+
+
+setTimeout(()=>{
+    console.log("Print after 2 seconds")
+},2000)
+
+
+setTimeout(()=>{
+    console.log("Print after zero seconds")
+},0)
+
+console.log("Finishing Up")
