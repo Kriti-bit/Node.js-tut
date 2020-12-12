57170aaa6e9e6aef219fd55f139b23cf9fe6eb81
Kriti <69788174+Kriti-bit@users.noreply.github.com>
2020-12-12 16:13:02 +0530

    Create forecast.js

---
+++ weather-app/web-server/search_form/forecast.js
@@ -0,0 +1,24 @@
+const request = require("request")
+
+const forecast=(address,callback)=>
+{
+    const url='Link'+encodeURIComponent(address)+''
+    request({ url : url, json:true}, (error,{body})=>{
+        if (error) {
+          callback('Unable to connect to API',undefined)
+        } else if (body.error){
+            callback('Unable to find data',undefined)
+        } else if(body.location.length === 0){
+            callback('Unable to connect. Try with another location',error)
+        } else {
+            callback(undefined,{
+                place_name:body.location.name,
+                temp: body.current.temp_c,
+                humidity:body.current.humidity //Instead of writing data.body again and again we simply pass body as the argument and access it directly.
+            })
+       
+        }
+    })
+}
+
+module.exports = forecast
