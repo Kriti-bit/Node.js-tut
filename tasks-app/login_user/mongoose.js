9ae1dfa229683a190fb4246b89110141f4410aea
Kriti <69788174+Kriti-bit@users.noreply.github.com>
2021-02-27 17:01:46 +0530

    Create mongoose.js

---
+++ tasks-app/login_user/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+//const validator=require('validator')
+
+mongoose.connect('LINK',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology: true, 
+    useFindAndModify:false 
+})
